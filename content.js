f14c7b144f7c52f2bd373479aa2b227ae75eba53
vitorboccio <vitorboccio@gmail.com>
2020-08-03 14:57:03 -0400

    saving playback rate on localstorage

--- content.js
+++ content.js
@@ -1,17 +1,36 @@
-const interval = setInterval(() => {
+const playBackRates = [1, 1.25, 1.5, 1.75, 2, 3]
+let playbackRate = 1;
+const getHeader = setInterval(() => {
     const header = document.querySelector('header');
+    
     if (header)  {
-        clearInterval();
-        const button = document.createElement("button");
-        button.innerHTML = "2x";
-        button.classList.add("timesButton");
-        header.appendChild(button);
-        
-        button.addEventListener("click", () => {
-            const audios = document.querySelectorAll("audio");
-            audios.forEach(audio => {
-                audio.playbackRate = 2;
-            })
+        clearInterval(getHeader);
+        const select = document.createElement("select");
+        select.id = 'rates'
+        playBackRates.forEach(rate => {
+            const option = document.createElement("option");
+            option.text = `${rate}x`;
+            option.setAttribute('value', rate); 
+            select.add(option);
         })
+        header.appendChild(select);
+        
+        select.addEventListener("change", playbackRateOption => {
+            const audios = document.querySelectorAll("audio");
+            playbackRate = playbackRateOption.target.value;
+            savePlaybackRateOnLocalStorage();
+            audios.forEach(audio => {
+                audio.playbackRate = playbackRate;
+            })
+        });
     };
 }, 1000);
+
+const savePlaybackRateOnLocalStorage = () => {
+    console.log('playbackRate', playbackRate);
+    window.localStorage.setItem('playbackRate', playbackRate);
+}
+
+const checkLocalStorage = setInterval(() => {
+    savePlaybackRateOnLocalStorage();
+}, 5000);
